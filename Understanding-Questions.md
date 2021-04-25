1fe18760d7b553d3560a59940ece51fcbb9ae861
Hamida Sh <sharifovahamida@gmail.com>
2021-04-24 21:23:51 -0700

    MVP

--- Understanding-Questions.md
+++ Understanding-Questions.md
@@ -1,7 +1,8 @@
 # Understanding Questions:
 1. What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
 * The user presses the 1 button.
-* 
-...
-
+* additionOne eventhandler in App.js is called, which dispatches addOne() function from action creator (which returns {type:ADD_ONE} action)
+* Action is started in reducer with passed ADD_ONE action creater type
+* reducer copies the state, and changes state.total by adding 1 to its current value
+* Change in the state.total, was passe to TotalDisplay from App.js and since the state changed, there was a render of the TotalDisplay component which displayed new state.total
 * TotalDisplay shows the total plus 1.
--- src/App.js
+++ src/App.js
@@ -1,62 +1,96 @@
-import React from 'react';
+import React, { useReducer } from "react";
+import reducer, { initialState } from "./reducers";
 
-import './App.css';
+import "./App.css";
 
-import TotalDisplay from './components/TotalDisplay';
-import CalcButton from './components/CalcButton';
+import TotalDisplay from "./components/TotalDisplay";
+import CalcButton from "./components/CalcButton";
+import {
+  addMem,
+  addMemDisplay,
+  addOne,
+  applyNumber,
+  changeOperation,
+  clearDisplay,
+  memClear,
+} from "./actions";
 
 function App() {
+  const [calcState, dispatch] = useReducer(reducer, initialState);
+
+  // const additionOne = (e) => {
+  //   e.preventDefault();
+  //   dispatch(addOne());
+  // }
+
+  const applySelectedNumber = (num) => dispatch(applyNumber(num));
+
+  const changeSelectedOper = (operation) =>
+    dispatch(changeOperation(operation));
+
+  const clearCalcDisplay = () => dispatch(clearDisplay());
+
+  const addCalcMem = () => dispatch(addMem());
+
+  const addCalcMemDisplay = () => dispatch(addMemDisplay());
+
+  const calcMemClear = () => dispatch(memClear());
 
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
-        <a className="navbar-brand" href="#"><img width="40px" src="./Lambda-Logo-Red.png"/> Lambda Reducer Challenge</a>
+        <a className="navbar-brand" href="#">
+          <img width="40px" src="./Lambda-Logo-Red.png" /> Lambda Reducer
+          Challenge
+        </a>
       </nav>
 
-      <div className = "container row mt-5">
+      <div className="container row mt-5">
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
-            
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={calcState.total} />
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
-            </div>
-            
-            <div className="row">
-              <CalcButton value={"M+"}/>
-              <CalcButton value={"MR"}/>
-              <CalcButton value={"MC"}/>
+              <span id="operation">
+                <b>Operation:</b> {calcState.operation}{" "}
+              </span>
+              <span id="memory">
+                <b>Memory:</b> {calcState.memory}{" "}
+              </span>
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton value={"M+"} onClick={() => addCalcMem()} />
+              <CalcButton value={"MR"} onClick={() => addCalcMemDisplay()}/>
+              <CalcButton value={"MC"} onClick={() => calcMemClear()}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton value={1} onClick={() => applySelectedNumber(1)} />
+              <CalcButton value={2} onClick={() => applySelectedNumber(2)} />
+              <CalcButton value={3} onClick={() => applySelectedNumber(3)} />
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton value={4} onClick={() => applySelectedNumber(4)} />
+              <CalcButton value={5} onClick={() => applySelectedNumber(5)} />
+              <CalcButton value={6} onClick={() => applySelectedNumber(6)} />
             </div>
 
             <div className="row">
-              <CalcButton value={"+"}/>
-              <CalcButton value={"*"}/>
-              <CalcButton value={"-"}/>
+              <CalcButton value={7} onClick={() => applySelectedNumber(7)} />
+              <CalcButton value={8} onClick={() => applySelectedNumber(8)} />
+              <CalcButton value={9} onClick={() => applySelectedNumber(9)} />
+            </div>
+
+            <div className="row">
+              <CalcButton value={"+"} onClick={() => changeSelectedOper("+")} />
+              <CalcButton value={"*"} onClick={() => changeSelectedOper("*")} />
+              <CalcButton value={"-"} onClick={() => changeSelectedOper("-")} />
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"}/>
+              <CalcButton value={"CE"} onClick={() => clearCalcDisplay()} />
             </div>
-
           </form>
         </div>
       </div>
